54b1b12cfd77336f962f1b6ba88c495a1ed4664a
Ritu Bafna <ritujane78@gmail.com>
2026-08-24 13:47:57 +0545

    initial commit

---
+++ src/test/java/com/jane/texttosql/TextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.texttosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
